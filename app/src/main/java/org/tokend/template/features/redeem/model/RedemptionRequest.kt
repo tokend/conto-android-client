81bd21d14214f3be3fe53e5f5ebc976e30f0d2d2
Oleg Koretsky <test.radiokot@gmail.com>
2019-06-24 15:58:58 +0300

    Added model for redemption request

---
+++ app/src/main/java/org/tokend/template/features/redeem/model/RedemptionRequest.kt
@@ -0,0 +1,109 @@
+package org.tokend.template.features.redeem.model
+
+import org.tokend.wallet.Base32Check
+import org.tokend.wallet.NetworkParams
+import org.tokend.wallet.Transaction
+import org.tokend.wallet.xdr.*
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.math.BigDecimal
+
+/**
+ * Holds data required to create and submit
+ * payment transaction for asset redemption.
+ */
+class RedemptionRequest(
+        val sourceAccountId: String,
+        val assetCode: String,
+        val amount: BigDecimal,
+        val salt: Long,
+        val timeBounds: TimeBounds,
+        val signature: DecoratedSignature
+) {
+    fun serialize(networkParams: NetworkParams): ByteArray {
+        val byteStream = ByteArrayOutputStream()
+        DataOutputStream(byteStream).use { stream ->
+            stream.write(Base32Check.decodeAccountId(sourceAccountId))
+            stream.writeInt(assetCode.length)
+            stream.write(assetCode.toByteArray(STRING_CHARSET))
+            stream.writeLong(networkParams.amountToPrecised(amount))
+            stream.writeLong(salt)
+            stream.writeLong(timeBounds.minTime)
+            stream.writeLong(timeBounds.maxTime)
+            stream.write(signature.hint.wrapped)
+            stream.write(signature.signature)
+        }
+        return byteStream.toByteArray()
+    }
+
+    companion object {
+        private val STRING_CHARSET = Charsets.UTF_8
+
+        fun fromTransaction(transaction: Transaction,
+                            assetCode: String): RedemptionRequest {
+            val op = (transaction
+                    .operations
+                    .firstOrNull()
+                    ?.body
+                    as? Operation.OperationBody.Payment)
+                    ?.paymentOp
+                    ?.takeIf { transaction.operations.size == 1 }
+                    ?: throw IllegalArgumentException(
+                            "Redemption transaction must have only a single payment operation"
+                    )
+
+            val sourceAccountId = (transaction.sourceAccountId as? PublicKey.KeyTypeEd25519)
+                    ?.ed25519
+                    ?.wrapped
+                    ?: throw IllegalArgumentException("Unknown source account public key type")
+
+            val signature = transaction
+                    .signatures
+                    .firstOrNull()
+                    .takeIf { transaction.signatures.size == 1 }
+                    ?: throw IllegalArgumentException(
+                            "Redemption transaction must have only a single signature"
+                    )
+
+            return RedemptionRequest(
+                    sourceAccountId = Base32Check.encodeAccountId(
+                            sourceAccountId
+                    ),
+                    assetCode = assetCode,
+                    salt = transaction.salt,
+                    timeBounds = transaction.timeBounds,
+                    amount = transaction.networkParams.amountFromPrecised(op.amount),
+                    signature = signature
+            )
+        }
+
+        fun fromSerialized(networkParams: NetworkParams,
+                           serializedRequest: ByteArray): RedemptionRequest {
+            DataInputStream(ByteArrayInputStream(serializedRequest)).use { stream ->
+                return RedemptionRequest(
+                        sourceAccountId = Base32Check.encodeAccountId(
+                                ByteArray(32).also { stream.read(it) }
+                        ),
+                        assetCode = String(
+                                ByteArray(stream.readInt()).also { stream.read(it) },
+                                STRING_CHARSET
+                        ),
+                        amount = networkParams.amountFromPrecised(stream.readLong()),
+                        salt = stream.readLong(),
+                        timeBounds = TimeBounds(
+                                minTime = stream.readLong(),
+                                maxTime = stream.readLong()
+                        ),
+                        signature = DecoratedSignature(
+                                hint = XdrByteArrayFixed4(
+                                        ByteArray(4).also { stream.read(it) }
+                                ),
+                                signature = ByteArray(stream.available()).also { stream.read(it) }
+                        )
+                )
+            }
+        }
+    }
+}
---
+++ app/src/test/java/org/tokend/template/test/RedemptionTest.kt
@@ -0,0 +1,87 @@
+package org.tokend.template.test
+
+import org.junit.Assert
+import org.junit.FixMethodOrder
+import org.junit.Test
+import org.junit.runners.MethodSorters
+import org.tokend.template.features.redeem.model.RedemptionRequest
+import org.tokend.wallet.*
+import org.tokend.wallet.xdr.*
+import java.math.BigDecimal
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class RedemptionTest {
+    @Test
+    fun aSerializeDeserializeRequest() {
+        val request = RedemptionRequest(
+                sourceAccountId = Account.random().accountId,
+                assetCode = "OLE",
+                amount = BigDecimal("1.5"),
+                salt = 123L,
+                timeBounds = TimeBounds(0, 999L),
+                signature = DecoratedSignature(
+                        hint = XdrByteArrayFixed4(byteArrayOf(1, 2, 3, 4)),
+                        signature = byteArrayOf(1, 2, 3, 4, 3, 2, 1)
+                )
+        )
+
+        val netParams = NetworkParams("")
+
+        val serialized = request.serialize(netParams)
+        val deserialized = RedemptionRequest.fromSerialized(netParams, serialized)
+
+        Assert.assertEquals(request.sourceAccountId, deserialized.sourceAccountId)
+        Assert.assertEquals(request.assetCode, deserialized.assetCode)
+        Assert.assertEquals(request.amount, deserialized.amount)
+        Assert.assertEquals(request.salt, deserialized.salt)
+        Assert.assertEquals(request.timeBounds.minTime, deserialized.timeBounds.minTime)
+        Assert.assertEquals(request.timeBounds.maxTime, deserialized.timeBounds.maxTime)
+        Assert.assertArrayEquals(request.signature.hint.wrapped, deserialized.signature.hint.wrapped)
+        Assert.assertArrayEquals(request.signature.signature, deserialized.signature.signature)
+    }
+
+    @Test
+    fun bRequestCreationFromTransaction() {
+        val netParams = NetworkParams("")
+        val account = Account.random()
+        val sourceAccountId = account.accountId
+        val sourceBalanceId = Base32Check.encodeBalanceId(ByteArray(32))
+        val amount = BigDecimal("1.5")
+        val destinationAccountId = Base32Check.encodeAccountId(ByteArray(32))
+        val assetCode = "OLE"
+
+        val emptyFee = Fee(0L, 0L, Fee.FeeExt.EmptyVersion())
+
+        val op = PaymentOp(
+                sourceBalanceID = PublicKeyFactory.fromBalanceId(sourceBalanceId),
+                amount = netParams.amountToPrecised(amount),
+                destination = PaymentOp.PaymentOpDestination.Account(
+                        PublicKeyFactory.fromAccountId(destinationAccountId)
+                ),
+                feeData = PaymentFeeData(emptyFee, emptyFee, false,
+                        PaymentFeeData.PaymentFeeDataExt.EmptyVersion()),
+                reference = "",
+                subject = "",
+                ext = PaymentOp.PaymentOpExt.EmptyVersion()
+        )
+
+        val tx = TransactionBuilder(netParams, sourceAccountId)
+                .addOperation(Operation.OperationBody.Payment(op))
+                .addSigner(account)
+                .build()
+
+        val salt = tx.salt
+        val timeBounds = tx.timeBounds
+
+        val request = RedemptionRequest.fromTransaction(tx, assetCode)
+
+        Assert.assertEquals(sourceAccountId, request.sourceAccountId)
+        Assert.assertEquals(assetCode, request.assetCode)
+        Assert.assertEquals(amount, request.amount)
+        Assert.assertEquals(salt, request.salt)
+        Assert.assertEquals(timeBounds.minTime, request.timeBounds.minTime)
+        Assert.assertEquals(timeBounds.maxTime, request.timeBounds.maxTime)
+        Assert.assertArrayEquals(tx.signatures[0].hint.wrapped, request.signature.hint.wrapped)
+        Assert.assertArrayEquals(tx.signatures[0].signature, request.signature.signature)
+    }
+}
