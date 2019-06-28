519f65583acd4ebe20ca474598e45728775f0af7
Aleksandr <al.ivanchukov@gmail.com>
2019-06-28 13:00:34 +0300

    Pre fill redeem amount field

--- app/src/main/java/org/tokend/template/features/redeem/create/view/RedemptionAmountInputFragment.kt
+++ app/src/main/java/org/tokend/template/features/redeem/create/view/RedemptionAmountInputFragment.kt
@@ -1,12 +1,20 @@
 package org.tokend.template.features.redeem.create.view
 
 import android.os.Bundle
+import kotlinx.android.synthetic.main.fragment_amount_input.*
 import org.tokend.template.features.amountscreen.view.AmountInputFragment
 
 class RedemptionAmountInputFragment: AmountInputFragment() {
     override fun getTitleText(): String? = null
 
+    override fun onInitAllowed() {
+        super.onInitAllowed()
+        amount_edit_text.setText(PRE_FILLED_AMOUNT)
+    }
+
     companion object {
+        private const val PRE_FILLED_AMOUNT = "1"
+
         fun newInstance(assetCode: String): RedemptionAmountInputFragment {
             val fragment = RedemptionAmountInputFragment()
             fragment.arguments = Bundle().apply {
