3fb032af7dbd854eb830c83ef7449cd45be442c2
Oleg Koretsky <test.radiokot@gmail.com>
2019-07-12 18:31:27 +0300

    Added clients display (mocked data)

--- app/src/main/java/org/tokend/template/features/clients/repository/CompanyClientsRepository.kt
+++ app/src/main/java/org/tokend/template/features/clients/repository/CompanyClientsRepository.kt
@@ -1,16 +1,20 @@
 package org.tokend.template.features.clients.repository
 
 import io.reactivex.Single
+import io.reactivex.rxkotlin.toSingle
 import org.tokend.rx.extensions.toSingle
 import org.tokend.sdk.api.base.model.DataPage
 import org.tokend.sdk.api.base.params.PagingOrder
 import org.tokend.sdk.api.base.params.PagingParamsV2
 import org.tokend.sdk.api.integrations.dns.params.ClientsPageParams
+import org.tokend.template.data.model.SimpleAsset
 import org.tokend.template.data.repository.base.RepositoryCache
 import org.tokend.template.data.repository.base.pagination.PagedDataRepository
 import org.tokend.template.di.providers.ApiProvider
 import org.tokend.template.di.providers.WalletInfoProvider
 import org.tokend.template.features.clients.model.CompanyClientRecord
+import java.math.BigDecimal
+import java.util.concurrent.TimeUnit
 
 class CompanyClientsRepository(
         private val apiProvider: ApiProvider,
@@ -19,6 +23,22 @@ class CompanyClientsRepository(
 ) : PagedDataRepository<CompanyClientRecord>(itemsCache) {
 
     override fun getPage(nextCursor: String?): Single<DataPage<CompanyClientRecord>> {
+        val balances = listOf(
+                CompanyClientRecord.Balance(BigDecimal("0.5"), SimpleAsset("ECO")),
+                CompanyClientRecord.Balance(BigDecimal("0.009"), SimpleAsset("KMP")),
+                CompanyClientRecord.Balance(BigDecimal("1.43"), SimpleAsset("GH")),
+                CompanyClientRecord.Balance(BigDecimal("5"), SimpleAsset("MBR")),
+                CompanyClientRecord.Balance(BigDecimal("344.2"), SimpleAsset("JNU")),
+                CompanyClientRecord.Balance(BigDecimal("6.483"), SimpleAsset("GBI"))
+        )
+        return listOf(
+                CompanyClientRecord("0", "ole@mail.com", balances.shuffled()),
+                CompanyClientRecord("1", "bob@mail.com", balances.subList(0, 2)),
+                CompanyClientRecord("2", "dh@gmail.com", listOf())
+
+        ).toSingle().map { DataPage(null, it, true) }
+                .delay(1, TimeUnit.SECONDS)
+
         val accountId = walletInfoProvider.getWalletInfo()?.accountId
                 ?: return Single.error(IllegalStateException("No wallet info found"))
 
--- app/src/main/java/org/tokend/template/features/clients/view/CompanyClientsFragment.kt
+++ app/src/main/java/org/tokend/template/features/clients/view/CompanyClientsFragment.kt
@@ -1,20 +1,29 @@
 package org.tokend.template.features.clients.view
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.subjects.BehaviorSubject
+import kotlinx.android.synthetic.main.appbar.*
 import kotlinx.android.synthetic.main.fragment_company_clients.*
+import kotlinx.android.synthetic.main.include_appbar_elevation.*
+import kotlinx.android.synthetic.main.include_error_empty_view.*
 import kotlinx.android.synthetic.main.toolbar.*
 import org.tokend.template.R
 import org.tokend.template.features.clients.repository.CompanyClientsRepository
+import org.tokend.template.features.clients.view.adapter.CompanyClientItemsAdapter
+import org.tokend.template.features.clients.view.adapter.CompanyClientListItem
 import org.tokend.template.fragments.BaseFragment
 import org.tokend.template.fragments.ToolbarProvider
 import org.tokend.template.util.ObservableTransformers
+import org.tokend.template.view.util.ColumnCalculator
+import org.tokend.template.view.util.ElevationUtil
 import org.tokend.template.view.util.LoadingIndicatorManager
 
 class CompanyClientsFragment : BaseFragment(), ToolbarProvider {
@@ -28,6 +37,9 @@ class CompanyClientsFragment : BaseFragment(), ToolbarProvider {
     private val clientsRepository: CompanyClientsRepository
         get() = repositoryProvider.companyClients()
 
+    private lateinit var adapter: CompanyClientItemsAdapter
+    private lateinit var layoutManager: GridLayoutManager
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_company_clients, container, false)
     }
@@ -35,6 +47,7 @@ class CompanyClientsFragment : BaseFragment(), ToolbarProvider {
     override fun onInitAllowed() {
         initToolbar()
         initSwipeRefresh()
+        initList()
 
         subscribeToClients()
 
@@ -45,11 +58,28 @@ class CompanyClientsFragment : BaseFragment(), ToolbarProvider {
     private fun initToolbar() {
         toolbar.title = getString(R.string.clients_title)
         toolbarSubject.onNext(toolbar)
+        ElevationUtil.initScrollElevation(appbar, appbar_elevation_view)
     }
 
     private fun initSwipeRefresh() {
         swipe_refresh.setColorSchemeColors(ContextCompat.getColor(context!!, R.color.accent))
         swipe_refresh.setOnRefreshListener { update(force = true) }
+    }
+
+    private fun initList() {
+        adapter = CompanyClientItemsAdapter(amountFormatter)
+        layoutManager = GridLayoutManager(requireContext(), 1)
+        updateListColumnsCount()
+        clients_list.adapter = adapter
+        clients_list.layoutManager = layoutManager
+        //clients_list.addOnScrollListener(hideFabScrollListener)
+        adapter.onItemClick { _, item ->
+            // TODO
+        }
+
+        error_empty_view.observeAdapter(adapter, R.string.no_clients)
+        error_empty_view.setEmptyViewDenial { clientsRepository.isNeverUpdated }
+        error_empty_view.setEmptyDrawable(R.drawable.ic_accounts)
     }
     // endregion
 
@@ -59,6 +89,31 @@ class CompanyClientsFragment : BaseFragment(), ToolbarProvider {
                 .compose(ObservableTransformers.defaultSchedulers())
                 .subscribe { loadingIndicator.setLoading(it) }
                 .addTo(compositeDisposable)
+
+        clientsRepository
+                .itemsSubject
+                .compose(ObservableTransformers.defaultSchedulers())
+                .subscribe { displayClients() }
+                .addTo(compositeDisposable)
+
+        clientsRepository
+                .errorsSubject
+                .compose(ObservableTransformers.defaultSchedulers())
+                .subscribe { error ->
+                    if (!adapter.hasData) {
+                        error_empty_view.showError(error, errorHandlerFactory.getDefault()) {
+                            update(true)
+                        }
+                    } else {
+                        errorHandlerFactory.getDefault().handle(error)
+                    }
+                }
+                .addTo(compositeDisposable)
+    }
+
+    private fun displayClients() {
+        val clients = clientsRepository.itemsList
+        adapter.setData(clients.map(::CompanyClientListItem))
     }
 
     private fun update(force: Boolean = false) {
@@ -67,6 +122,16 @@ class CompanyClientsFragment : BaseFragment(), ToolbarProvider {
         } else {
             clientsRepository.update()
         }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        updateListColumnsCount()
+    }
+
+    private fun updateListColumnsCount() {
+        layoutManager.spanCount = ColumnCalculator.getColumnCount(requireActivity())
+        adapter.drawDividers = layoutManager.spanCount == 1
     }
 
     companion object {
