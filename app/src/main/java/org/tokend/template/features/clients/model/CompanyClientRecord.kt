9fc4c2a854fa6da9f44385fc745f5ed7d0f07855
Oleg Koretsky <test.radiokot@gmail.com>
2019-07-15 12:40:06 +0300

    Added company client status parsing

--- app/src/main/java/org/tokend/template/features/clients/model/CompanyClientRecord.kt
+++ app/src/main/java/org/tokend/template/features/clients/model/CompanyClientRecord.kt
@@ -2,21 +2,30 @@ package org.tokend.template.features.clients.model
 
 import org.tokend.sdk.api.integrations.dns.model.ClientResource
 import org.tokend.template.data.model.Asset
+import java.io.Serializable
 import java.math.BigDecimal
 
 class CompanyClientRecord(
         val accountId: String,
         val email: String,
+        val status: Status,
         val balances: List<Balance>
-) {
+) : Serializable {
     class Balance(
             val amount: BigDecimal,
             val asset: Asset
-    )
+    ) : Serializable
 
-    constructor(source: ClientResource): this(
+    enum class Status {
+        NOT_REGISTERED,
+        ACTIVE,
+        BLOCKED
+    }
+
+    constructor(source: ClientResource) : this(
             accountId = source.accountId,
             email = source.email,
+            status = Status.valueOf(source.status.toUpperCase()),
             balances = listOf()
     )
 }
