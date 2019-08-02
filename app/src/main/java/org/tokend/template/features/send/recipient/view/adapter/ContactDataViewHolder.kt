6f91516848d0a026cceb23c283fa0b966472f68b
Aleksandr <al.ivanchukov@gmail.com>
2019-08-02 17:32:31 +0300

    Display contacts phone numbers (#27)

--- app/src/main/java/org/tokend/template/features/send/recipient/view/adapter/ContactDataViewHolder.kt
+++ app/src/main/java/org/tokend/template/features/send/recipient/view/adapter/ContactDataViewHolder.kt
@@ -2,13 +2,13 @@ package org.tokend.template.features.send.recipient.view.adapter
 
 import android.view.View
 import kotlinx.android.synthetic.main.item_email.view.*
-import org.tokend.template.features.send.recipient.model.ContactEmail
+import org.tokend.template.features.send.recipient.model.ContactData
 import org.tokend.template.view.adapter.base.BaseViewHolder
 
-class EmailViewHolder(itemView: View) : BaseViewHolder<Any>(itemView) {
+class ContactDataViewHolder(itemView: View) : BaseViewHolder<Any>(itemView) {
 
     override fun bind(item: Any) {
-        item as ContactEmail
-        itemView.email_text_view.text = item.email
+        item as ContactData
+        itemView.email_text_view.text = item.data
     }
 }
--- app/src/main/java/org/tokend/template/features/send/recipient/view/adapter/ContactListItem.kt
+++ app/src/main/java/org/tokend/template/features/send/recipient/view/adapter/ContactListItem.kt
@@ -1,19 +1,19 @@
 package org.tokend.template.features.send.recipient.view.adapter
 
 import org.tokend.template.features.send.recipient.model.Contact
-import org.tokend.template.features.send.recipient.model.ContactEmail
+import org.tokend.template.features.send.recipient.model.ContactData
 
 class ContactListItem(
         val id: String,
         val name: String,
-        val emails: List<ContactEmail>,
+        val data: List<ContactData>,
         val photoUri: String?,
         var isExpanded: Boolean
 ) {
     constructor(source: Contact): this(
             id = source.id,
             name = source.name,
-            emails = source.emails,
+            data = source.data,
             photoUri = source.photoUri,
             isExpanded = false
     )
