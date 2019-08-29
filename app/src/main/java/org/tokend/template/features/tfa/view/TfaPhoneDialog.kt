3c5abc37fa63f5732923cf6e6593a31147f50afe
Oleg Koretsky <test.radiokot@gmail.com>
2019-08-29 13:58:49 +0300

    Removed input filters from phone 2FA dialog

--- app/src/main/java/org/tokend/template/features/tfa/view/TfaPhoneDialog.kt
+++ app/src/main/java/org/tokend/template/features/tfa/view/TfaPhoneDialog.kt
@@ -1,6 +1,7 @@
 package org.tokend.template.features.tfa.view
 
 import android.content.Context
+import android.text.InputType
 import org.tokend.sdk.tfa.TfaVerifier
 import org.tokend.template.R
 import org.tokend.template.util.errorhandler.ErrorHandler
@@ -13,9 +14,18 @@ class TfaPhoneDialog(context: Context,
                      tfaVerifierInterface: TfaVerifier.Interface)
     : TfaOtpDialog(context, errorHandler, tfaVerifierInterface) {
 
+    override fun beforeDialogShow() {
+        super.beforeDialogShow()
+
+        inputEditText.apply {
+            filters = arrayOf()
+            inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+        }
+    }
+
     override fun getMessage(): String {
         return context.getString(R.string.sms_tfa_dialog_message)
     }
 
-    override fun getMaxCodeLength(): Int = 6
+    override fun getMaxCodeLength(): Int = -1
 }
