30ea1977923072cb41c7dc5f49efa2840e6b6fdd
Oleg Koretsky <test.radiokot@gmail.com>
2019-08-30 19:12:29 +0300

    Moved plus-minus amount field logic to a separate view

--- app/src/main/java/org/tokend/template/features/wallet/view/SimpleBalanceDetailsActivity.kt
+++ app/src/main/java/org/tokend/template/features/wallet/view/SimpleBalanceDetailsActivity.kt
@@ -4,15 +4,10 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
-import android.view.MotionEvent
-import android.view.View
-import io.reactivex.Observable
-import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.addTo
 import kotlinx.android.synthetic.main.activity_simple_balance_details.*
 import kotlinx.android.synthetic.main.toolbar.*
 import org.jetbrains.anko.textColor
-import org.tokend.sdk.utils.BigDecimalUtil
 import org.tokend.template.R
 import org.tokend.template.activities.BaseActivity
 import org.tokend.template.data.model.BalanceRecord
@@ -21,17 +16,13 @@ import org.tokend.template.extensions.onEditorAction
 import org.tokend.template.util.Navigator
 import org.tokend.template.util.ObservableTransformers
 import org.tokend.template.view.util.LoadingIndicatorManager
-import org.tokend.template.view.util.input.AmountEditTextWrapper
 import java.math.BigDecimal
-import java.util.concurrent.TimeUnit
 
 class SimpleBalanceDetailsActivity : BaseActivity() {
     private val loadingIndicator = LoadingIndicatorManager(
             showLoading = { swipe_refresh.isRefreshing = true },
             hideLoading = { swipe_refresh.isRefreshing = false }
     )
-
-    private lateinit var amountWrapper: AmountEditTextWrapper
 
     private var defaultAvailableLabelColor: Int = 0
     private val errorColor: Int by lazy {
@@ -89,41 +80,13 @@ class SimpleBalanceDetailsActivity : BaseActivity() {
     private fun initFields() {
         defaultAvailableLabelColor = available_text_view.currentTextColor
 
-        amountWrapper = AmountEditTextWrapper(amount_edit_text)
-        amountWrapper.onAmountChanged { _, _ -> onAmountChanged() }
-
-        amount_edit_text.onEditorAction(this::tryToSend)
+        payment_amount_view.amountWrapper.onAmountChanged { _, _ -> onAmountChanged() }
+        payment_amount_view.editText.onEditorAction(this::tryToSend)
 
         preFillAmount()
     }
 
     private fun initButtons() {
-        val stoppingTouchListener: (View, MotionEvent) -> Boolean = { _, event ->
-            if (event.action == MotionEvent.ACTION_UP
-                    || event.action == MotionEvent.ACTION_CANCEL) {
-                stopAmountChange()
-            }
-            false
-        }
-
-        dec_amount_button.setOnClickListener {
-            changeAmount(inc = false)
-        }
-        dec_amount_button.setOnLongClickListener {
-            startAmountChange(inc = false)
-            true
-        }
-        dec_amount_button.setOnTouchListener(stoppingTouchListener)
-
-        inc_amount_button.setOnClickListener {
-            changeAmount(inc = true)
-        }
-        inc_amount_button.setOnLongClickListener {
-            startAmountChange(inc = true)
-            true
-        }
-        inc_amount_button.setOnTouchListener(stoppingTouchListener)
-
         send_button.setOnClickListener { tryToSend() }
     }
 
@@ -157,6 +120,7 @@ class SimpleBalanceDetailsActivity : BaseActivity() {
 
     private fun onBalancesUpdated() {
         displayBalance()
+        updateAmountMaximum()
         updateAmountError()
         updateSendAvailability()
     }
@@ -174,7 +138,7 @@ class SimpleBalanceDetailsActivity : BaseActivity() {
 
     // region Amount
     private fun preFillAmount() {
-        setAmount(
+        payment_amount_view.amountWrapper.setAmount(
                 if (PRE_FILLED_AMOUNT > balance.available)
                     BigDecimal.ZERO
                 else
@@ -182,63 +146,17 @@ class SimpleBalanceDetailsActivity : BaseActivity() {
         )
     }
 
-    private fun setAmount(amount: BigDecimal) {
-        amount_edit_text.setText(BigDecimalUtil.toPlainString(amount))
-        amount_edit_text.setSelection(amount_edit_text.text?.length ?: 0)
-    }
-
-    /**
-     * @return false if this change can't be applied
-     */
-    private fun changeAmount(inc: Boolean): Boolean {
-        val newAmount = amountWrapper.scaledAmount + if (inc) BigDecimal.ONE else MINUS_ONE
-
-        return if (newAmount.signum() < 0) {
-            setAmount(BigDecimal.ZERO)
-            return false
-        } else if (newAmount > balance.available) {
-            setAmount(balance.available)
-            false
-        } else {
-            setAmount(newAmount)
-            true
-        }
-    }
-
-    private var amountChangeDisposable: Disposable? = null
-    private fun startAmountChange(inc: Boolean, interval: Long = INITIAL_AMOUNT_CHANGE_INTERVAL_MS) {
-        stopAmountChange()
-        amountChangeDisposable = Observable
-                .interval(interval, TimeUnit.MILLISECONDS)
-                .compose(ObservableTransformers.defaultSchedulers())
-                .subscribe {
-                    val elapsed = it * interval
-                    if (interval < INITIAL_AMOUNT_CHANGE_INTERVAL_MS
-                            || elapsed <= MS_TO_AMOUNT_CHANGE_INTERVAL_DECREASE) {
-                        if (!changeAmount(inc)) {
-                            stopAmountChange()
-                        }
-                    } else {
-                        startAmountChange(
-                                inc,
-                                interval - AMOUNT_CHANGE_INTERVAL_DECREASE_STEP
-                        )
-                    }
-                }
-                .addTo(compositeDisposable)
-    }
-
-    private fun stopAmountChange() {
-        amountChangeDisposable?.dispose()
-    }
-
     private fun onAmountChanged() {
         updateAmountError()
         updateSendAvailability()
     }
 
+    private fun updateAmountMaximum() {
+        payment_amount_view.maxAmount = balance.available
+    }
+
     private fun updateAmountError() {
-        if (amountWrapper.scaledAmount > balance.available) {
+        if (payment_amount_view.amountWrapper.scaledAmount > balance.available) {
             available_text_view.textColor = errorColor
         } else {
             available_text_view.textColor = defaultAvailableLabelColor
@@ -248,7 +166,7 @@ class SimpleBalanceDetailsActivity : BaseActivity() {
 
     // region Send
     private fun updateSendAvailability() {
-        val amount = amountWrapper.scaledAmount
+        val amount = payment_amount_view.amountWrapper.scaledAmount
         canSend = amount.signum() > 0
                 && amount <= balance.available
     }
@@ -262,7 +180,7 @@ class SimpleBalanceDetailsActivity : BaseActivity() {
     private fun openSend() {
         Navigator.from(this).openSend(
                 asset = balance.assetCode,
-                amount = amountWrapper.scaledAmount,
+                amount = payment_amount_view.amountWrapper.scaledAmount,
                 requestCode = SEND_REQUEST
         )
     }
@@ -276,12 +194,8 @@ class SimpleBalanceDetailsActivity : BaseActivity() {
     // endregion
 
     companion object {
-        private val MINUS_ONE = BigDecimal.ZERO.dec()
         private val PRE_FILLED_AMOUNT = BigDecimal.ONE
         private const val BALANCE_ID_EXTRA = "balance_id"
-        private const val INITIAL_AMOUNT_CHANGE_INTERVAL_MS = 110L
-        private const val MS_TO_AMOUNT_CHANGE_INTERVAL_DECREASE = 2000L
-        private const val AMOUNT_CHANGE_INTERVAL_DECREASE_STEP = 50L
         private val SEND_REQUEST = "send".hashCode() and 0xffff
 
         fun getBundle(balanceId: String) = Bundle().apply {
