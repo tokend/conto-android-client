2fc410e06a181f9dffebe04882bec5779f4d8f3e
Oleg Koretsky <test.radiokot@gmail.com>
2019-09-05 14:16:52 +0300

    Removed balance changes bottom sheet from account QR screen

--- app/src/main/java/org/tokend/template/features/accountdetails/view/AccountDetailsFragment.kt
+++ app/src/main/java/org/tokend/template/features/accountdetails/view/AccountDetailsFragment.kt
@@ -1,19 +1,11 @@
 package org.tokend.template.features.accountdetails.view
 
 import android.os.Bundle
-import android.support.constraint.ConstraintLayout
-import android.support.design.widget.BottomSheetBehavior
-import android.support.design.widget.CoordinatorLayout
 import android.support.v4.content.ContextCompat
 import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.Space
 import android.widget.TextView
-import io.reactivex.rxkotlin.addTo
 import kotlinx.android.synthetic.main.activity_share_qr.*
-import kotlinx.android.synthetic.main.include_rounded_elevated_bottom_sheet_header.*
-import kotlinx.android.synthetic.main.layout_balance_changes_bottom_sheet.*
 import org.tokend.template.App
 import org.tokend.template.R
 import org.tokend.template.extensions.withArguments
@@ -36,60 +28,10 @@ class AccountDetailsFragment : ShareQrFragment() {
     override val shareDialogText: String
         get() = getString(R.string.share_account)
 
-    private lateinit var bottomSheet: BottomSheetBehaviorWithNestedScrollChild<*>
-
     override fun onInitAllowed() {
         super.onInitAllowed()
 
         initExtraActions()
-        initBottomSheet()
-    }
-
-    private fun initBottomSheet() {
-        val coordinatorLayout = CoordinatorLayout(requireContext())
-        coordinatorLayout.layoutParams = ConstraintLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_CONSTRAINT
-        ).apply {
-            topToBottom = R.id.appbar
-            bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID
-        }
-
-        layoutInflater.inflate(R.layout.layout_balance_changes_bottom_sheet,
-                coordinatorLayout, true)
-
-        root_layout.addView(coordinatorLayout)
-
-        bottomSheet = BottomSheetBehavior.from(balance_changes_bottom_sheet_layout)
-                as BottomSheetBehaviorWithNestedScrollChild<*>
-
-        // Fade header.
-        bottomSheet.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
-            override fun onStateChanged(p0: View, p1: Int) {}
-
-            override fun onSlide(p0: View, offset: Float) {
-                bottom_sheet_header_fade.alpha = offset
-            }
-        })
-
-        val fragment = BottomSheetAssetMovementsFragment()
-
-        fragment
-                .recyclerViewObservable
-                .subscribe(bottomSheet::setNestedScrollChild)
-                .addTo(compositeDisposable)
-
-        childFragmentManager
-                .beginTransaction()
-                .replace(R.id.fragment_container_layout, fragment)
-                .commit()
-
-        scrollable_root_layout.addView(
-                Space(requireContext()).apply {
-                    layoutParams = ViewGroup.LayoutParams(
-                            0, bottomSheet.peekHeight
-                    )
-                }
-        )
     }
 
     private fun initExtraActions() {
@@ -123,15 +65,6 @@ class AccountDetailsFragment : ShareQrFragment() {
             }
 
             scrollable_root_layout.addView(this)
-        }
-    }
-
-    override fun onBackPressed(): Boolean {
-        return if (bottomSheet.state == BottomSheetBehavior.STATE_EXPANDED) {
-            bottomSheet.state = BottomSheetBehavior.STATE_COLLAPSED
-            false
-        } else {
-            true
         }
     }
 
