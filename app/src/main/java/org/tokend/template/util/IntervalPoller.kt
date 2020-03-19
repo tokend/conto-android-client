dffe981a5f4278cbbb6171d4878e94698664f66d
Oleg Koretsky <test.radiokot@gmail.com>
2020-03-19 11:34:24 +0200

    Make interval poller emit item immediately

--- app/src/main/java/org/tokend/template/util/IntervalPoller.kt
+++ app/src/main/java/org/tokend/template/util/IntervalPoller.kt
@@ -1,8 +1,10 @@
 package org.tokend.template.util
 
+import io.reactivex.Completable
 import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.functions.BiFunction
+import io.reactivex.subjects.PublishSubject
 import java.util.concurrent.TimeUnit
 
 /**
@@ -14,14 +16,16 @@ import java.util.concurrent.TimeUnit
  *
  * @param deferredDataSource request for the data to poll which can be repeated
  * @param nonFatalErrorFilter must return true if data request error is not fatal and polling
- * can be continues
+ * can be continued
  */
-class IntervalPoller<T>(
+open class IntervalPoller<T>(
         val minInterval: Long,
         val timeUnit: TimeUnit,
         val deferredDataSource: Single<T>,
         val nonFatalErrorFilter: (Throwable) -> Boolean = { true }
 ) {
+    private enum class ResultState { AVAILABLE, NOT_AVAILABLE; }
+
     /**
      * @return [Single] that awaits for the first result
      */
@@ -30,33 +34,48 @@ class IntervalPoller<T>(
     }
 
     /**
+     * @return [Completable] that awaits for the first result
+     */
+    fun asCompletable(): Completable {
+        return asSingle().ignoreElement()
+    }
+
+    /**
      * @return [Observable] that emits all results
      */
     fun asObservable(): Observable<T> {
-        var lastResult: T? = null
+        val resultSubject = PublishSubject.create<T>()
+
         val pollingObservable = Observable.defer {
             deferredDataSource
                     .map {
-                        lastResult = it
-                        true
+                        resultSubject.onNext(it)
+                        ResultState.AVAILABLE
                     }
                     .onErrorResumeNext { error ->
                         if (nonFatalErrorFilter(error))
-                            Single.just(false)
+                            Single.just(ResultState.NOT_AVAILABLE)
                         else
                             Single.error(error)
                     }
-                    .onErrorReturnItem(false)
                     .toObservable()
         }
 
-        return Observable.zip(
-                pollingObservable,
-                Observable.timer(minInterval, timeUnit),
-                BiFunction { hasResult: Boolean, _: Any -> hasResult }
+        var isDisposed = false
+
+        val ticker =
+                Observable.zip(
+                        pollingObservable,
+                        Observable.timer(minInterval, timeUnit),
+                        BiFunction { hasResult: ResultState, _: Any -> hasResult }
+                ).repeatUntil { isDisposed }
+
+        return Observable.merge(
+                ticker,
+                resultSubject
         )
-                .repeat()
-                .filter { it }
-                .map { lastResult!! }
+                .filter { it !is ResultState }
+                .map { it as T }
+                .doOnDispose { isDisposed = true }
     }
 }
